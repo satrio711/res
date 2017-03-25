18edee448c7cf8ab59a7b9e1b5fc0ce88141bce4
satrio711 <satrio_yudo@yahoo.co.id>
2017-03-25 20:03:20 +0700

    Create HttpResponse.java

---
+++ HttpResponse.java
@@ -0,0 +1,14 @@
+package com.example.x450j.wifi;
+
+import java.net.URL;
+
+/**
+ * Created by X450J on 05/01/2017.
+ */
+public class HttpResponse {
+    private URL entity;
+
+    public URL getEntity() {
+        return entity;
+    }
+}
