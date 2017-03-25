89a64e0736b032e3462a6f9027c0093c274c18b8
satrio711 <satrio_yudo@yahoo.co.id>
2017-03-25 20:04:39 +0700

    Create HttpCliennt.java

---
+++ HttpCliennt.java
@@ -0,0 +1,10 @@
+package com.example.x450j.wifi;
+
+/**
+ * Created by X450J on 05/01/2017.
+ */
+public class HttpClient {
+    public HttpResponse execute(HttpPost httppost) {
+        return null;
+    }
+}
