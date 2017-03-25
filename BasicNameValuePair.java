83d3d856d4dcbdf65a356588b08367a05cae21e3
satrio711 <satrio_yudo@yahoo.co.id>
2017-03-25 20:06:02 +0700

    Create BasicNameValuePair.java

---
+++ BasicNameValuePair.java
@@ -0,0 +1,10 @@
+package com.example.x450j.wifi;
+
+/**
+ * Created by X450J on 05/01/2017.
+ */
+public class BasicNameValuePair extends NameValuePair {
+    public BasicNameValuePair(String req, String s) {
+        super();
+    }
+}
